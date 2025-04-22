6a65c4df16b70bd9e400eb39a4470877294a5ddc
Ganesh Nagilla <ganesh.nagilla@winjit.com>
2025-04-22 14:01:11 +0530

    remove insecure TLS override from cloudinaryConfig.js file

--- backend/cloudinaryConfig.js
+++ backend/cloudinaryConfig.js
@@ -2,9 +2,6 @@ import { v2 as cloudinary } from 'cloudinary';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import multer from 'multer';
 
-
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
